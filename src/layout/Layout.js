d3f6d17565548bf57da446f4e121d946b4be66b5
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-01 20:33:55 -0300

    Definido layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import reactDom from 'react-dom';
 
 class Layout extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            color: false
+        };
+
+        this.toggleColor = this.toggleColor.bind(this);
+    }
+
+    toggleColor() {
+        var color = this.state.color;
+        var paragraph = document.getElementById('paragraph');
+        if (color) {
+            reactDom.findDOMNode(paragraph).style.color = 'black';
+        } else {
+            reactDom.findDOMNode(paragraph).style.color = 'red';
+        }
+        this.setState({ color: !color });
+    }
+
     render() {
         return (
-            <p>Hola, mundo!</p>
+            <div>
+                <button class="btn btn-primary" onClick={this.toggleColor}>
+                    Toggle Color
+                </button>
+                <hr />
+                <p>Color?: {this.state.color ? 'true' : 'false'}</p>
+                <p id="paragraph">Paragraph</p>
+            </div>
         );
     }
 }
